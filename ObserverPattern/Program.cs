e09ddb8cdb4ca0965793cfcef60285806418c6e4
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-11 14:03:44 +0200

    Test removing observer

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -10,12 +10,15 @@ namespace ObserverPattern
             WeatherData weatherData = new WeatherData();
 
             // Create instances of displays
-            DisplayElement currentDisplay = new CurrentConditionDisplay(weatherData);
-            DisplayElement statisticsDisplay = new StatisticsDisplay(weatherData);
-            DisplayElement forecastDisplay = new ForecastDisplay(weatherData);
+            CurrentConditionDisplay currentDisplay = new(weatherData);
+            StatisticsDisplay statisticsDisplay = new(weatherData);
+            ForecastDisplay forecastDisplay = new(weatherData);
 
             weatherData.SetMeasurements(28, 65, 30.4f);
             weatherData.SetMeasurements(29, 70, 29.2f);
+
+            weatherData.RemoveObserver(forecastDisplay);
+
             weatherData.SetMeasurements(30, 90, 29.2f);
         }
     }
