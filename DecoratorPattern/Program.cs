d9acb1cf3fd5f28b5b1cad0a9364ac19eeda6c45
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-18 14:37:21 +0200

    Create all coffee mixes in Program.cs

--- DecoratorPattern/Program.cs
+++ DecoratorPattern/Program.cs
@@ -9,9 +9,35 @@ internal class Program
     {
         Dictionary<string, Beverage> beverages = new()
         {
-            ["espresso"] = new Espresso(),
-            ["lungo"] = new Water(new Espresso()),
-            ["americano"] = new Water(new Water(new Espresso()))
+            ["Espresso"] = new Espresso(),
+            ["Doppio"] = new Espresso(new Espresso()),
+            ["Lungo"] = new Water(new Espresso()),
+            ["Macchiato"] = new MilkFoam(new Espresso()),
+            ["Corretta"] = new Liquor(new Espresso()),
+            ["Con Panna"] = new Whip(new Espresso()),
+            ["Cappucinno"] = new MilkFoam(new SteamedMilk(new Espresso())),
+            ["Americano"] = new Water(new Water(new Espresso())),
+            ["Caffé Latte"] = new MilkFoam(new SteamedMilk(new SteamedMilk(new Espresso()))),
+            ["Flat White"] = new SteamedMilk(new SteamedMilk(new Espresso())),
+            ["Romana"] = new Lemon(new Espresso()),
+            ["Morocchino"] = new MilkFoam(new Chocolate(new Espresso())),
+            ["Mocha"] = new Whip(new SteamedMilk(new Chocolate(new Espresso()))),
+            ["Bicerin"] = new Whip(new WhiteChocolate(new BlackChocolate(new Espresso()))),
+            ["Breve"] = new HalfMilk(new MilkFoam(new Espresso())),
+            ["Raf coffee"] = new Cream(new VanillaSugar(new Espresso())),
+            ["Mead raf"] = new Cream(new Honey(new Espresso())),
+            ["Galao"] = new MilkFoam(new MilkFoam(new Espresso())),
+            ["Caffé affogato"] = new IceCream(new Espresso(new Espresso())),
+            ["Vienna coffee"] = new Whip(new Whip(new Espresso(new Espresso()))),
+            ["Glace"] = new IceCream(new Espresso()),
+            ["Chocolate milk"] = new Milk(new Milk(new Chocolate(new Water()))),
+            ["Demi – créme"] = new Cream(new Cream(new Espresso(new Espresso()))),
+            ["Latte macchiato"] = new MilkFoam(new SteamedMilk(new SteamedMilk(new Espresso()))),
+            ["Freddo"] = new Ice(new Liquor(new Espresso())),
+            ["Frappuccino"] = new Whip(new SteamedMilk(new Ice(new Espresso()))),
+            ["Caramel frappuccino"] = new Syrup(new Cream(new SteamedMilk(new Ice(new Espresso())))),
+            ["Frappe"] = new IceCream(new SteamedMilk(new SteamedMilk(new Espresso()))),
+            ["Irish Coffee"] = new Whip(new Whiskey(new Espresso(new Espresso())))
         };
 
         foreach (var kvp in beverages)
@@ -21,7 +47,6 @@ internal class Program
     }
 
     private static void PrintBeverage(string key, Beverage beverage)
-
     {
         Console.WriteLine($"{key}: {beverage.GetDescription()} ${beverage.cost():#.##}");
     }
